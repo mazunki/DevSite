03ceb060bc9837937d1d3c198b51e7ba9d53178b
mazunki <rolferen@gmail.com>
2019-11-20 10:23:24 +0100

    git has its own function

--- webserver.go
+++ webserver.go
@@ -16,13 +16,14 @@ var legalPage = map[string]bool {
 func main() {
 	fmt.Println("Listening on localhost:80")
 	http.HandleFunc("/", MainServer)
+	http.HandleFunc("/git/", GitServer)
 	http.ListenAndServe(":8080", nil)
 }
 
 func MainServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Tried to access ", r.URL.Path, " ...")
 	var p string
-	r1, _ := regexp.Compile("(?:^/(?P<path>(?:uio|git))?(?P<subpath>/?.*/)?(?P<site>.+(?P<filetype>\\.html|\\.css|\\.js)?)?$)")
+	r1 := regexp.MustCompile("(?:^/(?P<path>(?:uio))?(?P<subpath>/?.*/)?(?P<site>.+(?P<filetype>\\.html|\\.css|\\.js)?)?$)")
 	match := r1.FindStringSubmatch(r.URL.Path)
 	fullmatch, path, subpath, site, filetype := match[0], match[1], match[2], match[3], match[4]
 	orderedPath := strings.Split(subpath, "/")
@@ -30,25 +31,29 @@ func MainServer(w http.ResponseWriter, r *http.Request) {
 
 	if fullmatch == "/" {
 		p = "./content/index.html"
-		http.ServeFile(w, r, p)
 	} else if filetype == ".css" {
 		p = "./content/" + site
-		http.ServeFile(w, r, p)
-	} else if path == "git" {
-		fmt.Println(orderedPath)
-		fmt.Printf("(%s)", orderedPath[1])
-		if orderedPath[1] == "f" {
-			p = "https://github.com/mazunki/" + orderedPath[2] + "/blob/master/" + strings.Join(orderedPath[3:],"/" + "?raw=True")
-		} else {
-			p = "https://github.com/mazunki/"
-		}
-		http.Redirect(w, r, p, http.StatusSeeOther)
 	} else if legalPage[subpath + site] {
-		p = "./content" + site
-		http.ServeFile(w, r, p)
+		p = "./content" + strings.Join(orderedPath[2:],"/")
 	} else {
 		p = "./noncontent/404.html"
 		fmt.Println("Forbidden:", r.RemoteAddr)
-		http.ServeFile(w, r, p)
 	}
+	http.ServeFile(w, r, p)
+}
+
+func GitServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Git tried to access ", r.URL.Path, " ...")
+	var p string
+	orderedPath := strings.Split(r.URL.Path[1:], "/") // [git, f, <repo>, ...<path>]
+
+	if orderedPath[1] == "f" {
+		p = "https://github.com/mazunki/" + orderedPath[2] + "/blob/master/" + strings.Join(orderedPath[3:],"/") + "?raw=True"
+	} else if len(orderedPath)>2 {
+		p = "https://github.com/mazunki/" + orderedPath[1] + "/blob/master/" + strings.Join(orderedPath[2:],"/")
+	} else {
+		p = "https://github.com/mazunki/"
+	}
+	fmt.Println("Redirecting to", p)
+	http.Redirect(w, r, p, http.StatusSeeOther)
 }
